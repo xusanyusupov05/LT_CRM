3ea9a465a1ef92bdb50d90fa2a7e21c4f382e88a
xusanyusupov05 <xusanyusupov06@gmail.com>
2026-04-02 16:22:11 +0500

    feat: initialize application entry point and add Vercel analytics dependency

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import ProviderMain from './providers'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <ProviderMain/>
   </StrictMode>,
 )
